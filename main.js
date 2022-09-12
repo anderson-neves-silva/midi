d280a2a7a8e2d00b5965bdbf4b9e3e2ac50df3d5
Anderson Neves <andersonneves_@hotmail.com>
2022-09-11 23:09:29 -0300

    optimizing code with for repeat statement

--- main.js
+++ main.js
@@ -1,24 +1,21 @@
 function tocaSom (idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
-//aqui cria uma constante lista com todas as teclas
-const listaDeTeclas = document.querySelectorAll('.tecla');
-//criando uma referência variável
-let contador = 0;
 
-while (contador < listaDeTeclas.length) {
+const listaDeTeclas = document.querySelectorAll('.tecla'); //aqui cria uma constante lista com todas as teclas
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) { //para
     const tecla = listaDeTeclas[contador];    
     const instrumento = tecla.classList[1];
-    //aqui uso a forma template string
-    const idAudio = `#som_${instrumento}`;
-    //console.log(idAudio);   
-    //aqui crio uma função anônima
-    tecla.onclick = function () {
+    const idAudio = `#som_${instrumento}`; //aqui uso a forma template string
+    
+    tecla.onclick = function () { //aqui crio uma função anônima
         tocaSom(idAudio);
     }
-    contador =  contador + 1;
-    //console.log(contador);
+
 }
+
+
 
 /*
 como acessando os elementos do html diretamente quando não temos classes
